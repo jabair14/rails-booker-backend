8b9cb4ddec2e01ac423f6c6b65fbe0ae43be50b2
Radiyah <rsabur58@gmail.com>
2021-05-12 17:21:16 -0400

    formatting fix

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -2,33 +2,27 @@ class ClientsController < ApplicationController
 
     def index
         clients = Client.all 
-
         render json: clients, except: [:created_at, :updated_at]
     end
 
-    def show 
-
+    def show
         client = Client.find(params[:id])
-
         render json: client
 
     end
 
     def create
         client = Client.create!(client_params)
-
         render json: client 
     end
 
     def update
         client = Client.find(params[:id])
-
         client.update!(client_params)
     end
 
     def destroy
         client = Client.find(params[:id])
-
         client.destroy
     end
 
@@ -36,8 +30,6 @@ class ClientsController < ApplicationController
     private 
 
     def client_params
-    
         params.require(:client).permit(:name, :email, :username, :location, :avatar, :password)
-    
     end
 end
